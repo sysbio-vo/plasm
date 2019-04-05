9fafc54a770b54ff609df6d2d8e71bee1adde50a
aln <alnfgit@gmail.com>
2019-04-05 13:17:52 +0000

    development version of seurat

--- scripts/seurat.R
+++ scripts/seurat.R
@@ -2,45 +2,81 @@ library(Seurat)
 library(dplyr)
 
 # Load the PBMC dataset
-pbmc.data <- Read10X(data.dir = "../raws/test1_filtered_gene_bc_matrices_mex/GRCh38-1.2.0_premrna/")
+plc.data <- Read10X(data.dir = "../data/cellranger_short/Aggr-Term/")
+#plc.data <- Read10X(data.dir = "../data/cellranger_short/Aggr-Villi/")
+#plc.data <- Read10X(data.dir = "../data/cellranger_short/pbmc/")
 
-pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 3, min.genes = 200, 
+plc <- CreateSeuratObject(counts = plc.data, min.cells = 3, min.features = 200, 
                            project = "plasm")
 
 
 ### QC
 
-mito.genes <- grep(pattern = "^MT-", x = rownames(x = pbmc@data), value = TRUE)
-percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
+plc[["percent.mt"]] <- PercentageFeatureSet(object = plc, pattern = "^MT-")
+VlnPlot(object = plc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+plot1 <- FeatureScatter(object = plc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(object = plc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(plot1, plot2))
 
-# AddMetaData adds columns to object@meta.data, and is a great place to stash QC stats
-pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
-VlnPlot(object = pbmc, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
+plc <- subset(x = plc, subset = nFeature_RNA > 200 & nFeature_RNA < 2000)
+### scTransform
+#plc <- SCTransform(object = plc, verbose = FALSE)
+plc <- SCTransform(object = plc, vars.to.regress = "percent.mt", verbose = FALSE)
+### Filtering
+#plc <- subset(x = plc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
 
-par(mfrow = c(1, 2))
-GenePlot(object = pbmc, gene1 = "nUMI", gene2 = "percent.mito")
-GenePlot(object = pbmc, gene1 = "nUMI", gene2 = "nGene")
-
-# Filtering
-pbmc <- FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), 
-                    low.thresholds = c(200, -Inf), high.thresholds = c(4300, 0.05))
-
-### Nornalization
-pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
-                      scale.factor = 10000)
+### Normalization
+#plc <- NormalizeData(object = plc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 ### Variable genes
-pbmc <- FindVariableGenes(object = pbmc, mean.function = ExpMean, dispersion.function = LogVMR, 
-                          x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
-length(x = pbmc@var.genes)
+#plc <- FindVariableFeatures(object = plc, selection.method = "vst", nfeatures = 2000)
 
+### Scale
+#all.genes <- rownames(x = plc)
+#plc <- ScaleData(object = plc, features = all.genes)
 
-### Remove noise
+### Dimentionality reduction
+plc <- RunPCA(object = plc, features = VariableFeatures(object = plc))
+
+plc <- JackStraw(object = plc, num.replicate = 100)
+plc <- ScoreJackStraw(object = plc, dims = 1:20)
+JackStrawPlot(object = plc, dims = 1:15)
+
+### Clustering
+plc <- FindNeighbors(object = plc, dims = 1:30)
+plc <- FindClusters(object = plc, resolution = 0.5)
+
 gc()
-pbmc <- ScaleData(object = pbmc, vars.to.regress = c("nUMI", "percent.mito"))
+plc <- RunTSNE(object = plc, dims.use = 1:10, do.fast = TRUE)
+plc <- RunUMAP(object = plc, dims = 1:30)
+DimPlot(object = plc, reduction = "tsne", label = TRUE, pt.size = 0.5) + NoLegend()
+DimPlot(object = plc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 
-pbmc <- RunPCA(object = pbmc, pc.genes = marrow@var.genes, pcs.print = 1:4, 
-               genes.print = 10)
+##
+FeaturePlot(object = plc.tsne, features.plot = c("LYZ"), cols.use = c("grey", "blue"), 
+                              reduction.use = "tsne")
+FeaturePlot(object = plc.tsne, features.plot = c("THEMIS"), cols.use = c("grey", "blue"), 
+            reduction.use = "tsne")
 
-PCHeatmap(object = pbmc, pc.use = 4, do.balanced = TRUE, label.columns = FALSE, 
-          remove.key = TRUE)
+FeaturePlot(object = plc.tsne, features.plot = c("SPTA1"), cols.use = c("grey", "blue"), 
+            reduction.use = "tsne")
+FeaturePlot(object = plc.tsne, features.plot = c("DPYD"), cols.use = c("grey", "blue"), 
+            reduction.use = "tsne")
+##
+
+plc@meta.data$mito <- 0
+plc@meta.data[which(plc@meta.data$percent.mt>1),]$mito <- 1
+
+DimPlot(object = plc, reduction = "umap", label = TRUE, pt.size = 0.5, group.by = "mito", alpha=0.5)
+        + NoLegend()
+DimPlot(object = plc, reduction = "tsne", label = TRUE, pt.size = 0.5, group.by = "mito", alpha=0.5)
+        + NoLegend()
+
+
+plc@meta.data$doubl <- 0
+plc@meta.data[which(plc@meta.data$nFeature_RNA>1000),]$doubl <- 1
+
+DimPlot(object = plc, reduction = "umap", label = TRUE, pt.size = 0.5, group.by = "doubl", alpha=0.5)
++ NoLegend()
+DimPlot(object = plc, reduction = "tsne", label = TRUE, pt.size = 0.5, group.by = "doubl", alpha=0.5)
++ NoLegend()
