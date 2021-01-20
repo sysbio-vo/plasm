5ecdc6a8e3e4a79f5f5da087a35554d53c2b3fd2
aln <alnfgit@gmail.com>
2021-01-20 10:29:19 +0000

    old commit

--- scripts/seurat.R
+++ scripts/seurat.R
@@ -1,55 +1,51 @@
 library(Seurat)
+library(DoubletDecon)
 library(dplyr)
 
 # Load metadata
 
+samples <- read.table("../meta/samples.tsv", sep="\t", stringsAsFactors = F , header = T)
+
 #meta <- read.table("../meta/clusters_term_FH.tsv", sep="\t", stringsAsFactors = F, header = T)
-meta <- read.table("../meta/clusters_FH_villi.csv", sep=",", stringsAsFactors = F, header = T)
-# Load the PBMC dataset
-#plc.data <- Read10X(data.dir = "../data/cellranger_short/Aggr-Term/")
-plc.data <- Read10X(data.dir = "../data/cellranger_short/Aggr-Villi/")
-#plc.data <- Read10X(data.dir = "../data/cellranger_short/pbmc/")
+#meta <- read.table("../meta/clusters_FH_villi.csv", sep=",", stringsAsFactors = F, header = T)
+trims = c("first", "term")
+tissues = c("villi", "placenta", "decidua")
+trim = trims[2]
+tissue = tissues[2]
+diagnoses = c("PE", "C")
+diagnosis = diagnoses[2]
+
+# Load dataset
+ids <- samples[which((samples$Tissue==tissue) & (samples$Trimester==trim) & (samples$Diagnosis==diagnosis)),]$ID
+folders <- paste(ids, stringr::str_to_title(tissue), sep="-")
+
+plc.data <- Read10X_h5(paste("../data/CR3/", folders[1], "/filtered_feature_bc_matrix.h5", sep=""))
 
 plc <- CreateSeuratObject(counts = plc.data, min.cells = 3, min.features = 200, 
                            project = "plasm")
 
-cols <- colnames(plc@assays$RNA@counts)
-cols <- strsplit(cols, "-")
-cols <- sapply(cols, "[", 2)
+#cols <- colnames(plc@assays$RNA@counts)
+#cols <- strsplit(cols, "-")
+#cols <- sapply(cols, "[", 2)
+#colnames(plc@meta.data)
+#plc@meta.data$specimen <- cols
+#meta <- meta[which(meta$Barcode %in% colnames(plc)),]
 
-colnames(plc@meta.data)
-plc@meta.data$specimen <- cols
-
-meta <- meta[which(meta$Barcode %in% colnames(plc)),]
-
-plc@meta.data$clusters <- meta$Cluster[match(colnames(plc), meta$Barcode)]
-
-plc@meta.data$clusters[which(plc@meta.data$clusters=="")] <- "unknown"
+#plc@meta.data$clusters <- meta$Cluster[match(colnames(plc), meta$Barcode)]
+#plc@meta.data$clusters[which(plc@meta.data$clusters=="")] <- "unknown"
 
 ### QC
-
 plc[["percent.mt"]] <- PercentageFeatureSet(object = plc, pattern = "^MT-")
 VlnPlot(object = plc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 plot1 <- FeatureScatter(object = plc, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(object = plc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 CombinePlots(plots = list(plot1, plot2))
 
-plc <- subset(x = plc, subset = nFeature_RNA > 200 & nFeature_RNA < 2000 & percent.mt < 5)
+plc <- subset(x = plc, subset = nFeature_RNA > 200 & nFeature_RNA < 2000 & percent.mt < 1)
+
 ### scTransform
 plc <- SCTransform(object = plc, verbose = FALSE)
 #plc <- SCTransform(object = plc, vars.to.regress = "percent.mt", verbose = FALSE)
-### Filtering
-#plc <- subset(x = plc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
-
-### Normalization
-#plc <- NormalizeData(object = plc, normalization.method = "LogNormalize", scale.factor = 10000)
-
-### Variable genes
-#plc <- FindVariableFeatures(object = plc, selection.method = "vst", nfeatures = 2000)
-
-### Scale
-#all.genes <- rownames(x = plc)
-#plc <- ScaleData(object = plc, features = all.genes)
 
 ### Dimentionality reduction
 plc <- RunPCA(object = plc, features = VariableFeatures(object = plc))
